7e00930ff2e2ee115a6c7832d0d5261aa2d18cc5
catemohi <78559060+catemohi@users.noreply.github.com>
2026-06-19 11:55:19 +0000

    chore: lock development dependencies

--- tests/test_package.py
+++ tests/test_package.py
@@ -7,4 +7,3 @@ def test_package_has_version() -> None:
     """Проверяет, что пакет экспортирует строковую версию."""
     assert isinstance(pyis74.__version__, str)
     assert pyis74.__version__
-
